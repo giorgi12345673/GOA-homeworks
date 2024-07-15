b0d74b7cf962bf307744c15e3c67d89fc7389d84
g27596533@gmail.com <g27596533@gmail.com>
2024-07-15 18:19:33 +0400

    homework

---
+++ level 10/homework/homework.py
@@ -0,0 +1,19 @@
+# 1
+name = input ("giorgi miqautadze")
+print(name)
+
+# 2
+surname = input ("giorgi miqautadze")
+surname(name)
+birthday_card = input ("happy birthday, wish you the best.")
+
+
+# 3
+
+favorite_pet_color_car = input ("dog, fox, green, blue, black, bugatti.")
+
+# 4
+#ინფუთი ნიშნავს ნებისმიერი ბრძანების ამუსავება. აუთფუთი კი ბძანებიდან გამოტანილ იმფორმაციას.
+
+
+# 5  concatenation- არის შეეღთება ხაზების და იმორმაციის შეეღთება.
